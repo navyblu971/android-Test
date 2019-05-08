088e8d1d0080a43dee148d871f83e98358ceeca1
atexide fabrice <navyblu971@hotmail.com>
2019-05-08 21:21:33 +0200

    remove error

--- navigo/src/main/java/com/tgb/myapplication/MapsActivity.java
+++ navigo/src/main/java/com/tgb/myapplication/MapsActivity.java
@@ -17,7 +17,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentViewi(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
